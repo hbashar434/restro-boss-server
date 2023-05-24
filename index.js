98d81faf981921078fd5f3bb726730761b914d07
hbashar434 <h.bashar434@gmail.com>
2023-05-24 19:48:49 +0600

    initial

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uprfadf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    client.connect();
+    const menuCollection = client.db("restroDB").collection("menu");
+    const reviewCollection = client.db("restroDB").collection("reviews");
+    ///////////////////////////////////////////////////////////////////////
+
+    
+
+
+
+
+    ///////////////////////////////////////////////////////////////////////
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Boss has Arrived");
+});
+
+app.listen(port, () => {
+  console.log(`Restro boss has arrived at port ${port}`);
+});
